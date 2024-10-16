842571431221dcdf23ba5d937777406a166208a1
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2024-10-16 14:37:00 +0530

    Add /api/comic endpoint

---
+++ routes/api/comic/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+router.get('/', (req, res, next) => {
+    res.send({ status: "success", timestamp: Date.now() });
+});
+
+export default router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 
+import comicRouter from "./comic/index.js";
+
+router.use("/comic", comicRouter);
+
 router.get('/', (req, res, next) => {
   res.send({ status: "success", timestamp: Date.now() });
 });
